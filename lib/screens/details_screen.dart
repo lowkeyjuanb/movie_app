253ad079035407349c35679e18fa33a9d1eeb2de
Juan Barrera <this.is.juanb@gmail.com>
2024-05-22 15:10:20 -0700

    details_screen overview, poster and more

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,13 +1,124 @@
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 
 class DetailsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-              child: const Center(
-                child: Text('DetailsScreen'),),),
+        body: CustomScrollView(
+      slivers: [
+        _CustomAppBar(),
+        SliverList(
+            delegate:
+                SliverChildListDelegate([_PosterAndTitle(), 
+                _Overview(),
+                _Overview(),
+                _Overview(),
+                
+                ])),
+      ],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.green[900],
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          color: Colors.black12,
+          alignment: Alignment.bottomCenter,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: AssetImage('assets/no-image.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: AssetImage('assets/no-image.jpg'),
+              height: 150,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headlineMedium,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text('movie.originalTitle',
+                  style: textTheme.titleMedium,
+                  overflow: TextOverflow.ellipsis),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star_outline_outlined,
+                    size: 15,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'movie.voteAverage',
+                    style: textTheme.bodyMedium,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        child: Text(
+          'In nulla eiusmod nisi aliquip laborum. Irure minim aliqua incididunt sint esse ullamco aute eu. Ipsum amet cupidatat commodo nisi minim aliquip tempor culpa proident mollit ad qui aliqua.',
+          textAlign: TextAlign.justify,
+          style: Theme.of(context).textTheme.titleMedium,
+        ));
   }
 }
